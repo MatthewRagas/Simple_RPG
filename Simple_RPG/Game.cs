7222ea2bb8eab2cc6fa2806f1ad4c455cd4115a6
Matthew John Ragas <mjrag310@hotmail.com>
2019-08-21 11:15:16 -0500

    Added alive variable & create encounters till lose.

--- Simple_RPG/Game.cs
+++ Simple_RPG/Game.cs
@@ -8,18 +8,37 @@ namespace Simple_RPG
 {
     class Game
     {
+        string playerName = "";
+        int playerHealth = 100;
+        int playerAttack = 25;
+
+
         public void Start()
         {
-            string playerName = "";
-            int playerHealth = 100;
+            Welcome();
 
+            bool alive = true;
+
+           //fight until you lose;
+            while (alive)
+            {
+               alive = Encounter(13, 150);
+            }
+
+            Console.ReadKey();
+        }
+
+        void Welcome()
+        {
             //asks player for name. and assigns name to player;
             Console.WriteLine("What is your name? ");
             playerName = Console.ReadLine();
             Console.WriteLine("Welcome! " + playerName + ".");
-
-            //Monster Encounter
-            int monsterDamage = 13;
+        }
+        //Monster Encounter
+        bool Encounter(int monsterDamage, int monsterHealth)
+        {
+           
             Console.WriteLine("");
             Console.WriteLine("A wild thing appears!");
 
@@ -29,16 +48,34 @@ namespace Simple_RPG
 
             if (action == "fight" || action == "Fight")
             {
-                //monster attacks player;
-                Console.WriteLine("The  monster attacks! " + playerName + " takes "
-                    + monsterDamage + "damage.");
+                //loop of the fight option;
+                while(playerHealth > 0 && monsterHealth > 0)
+                {
+                    //monster attacks player;
+                    Console.WriteLine("The  monster attacks! " + playerName + " takes "
+                        + monsterDamage + "damage.");
 
-                playerHealth = playerHealth - monsterDamage;
+                    playerHealth -= monsterDamage;
 
-                Console.WriteLine(playerName + " has " + playerHealth +
-                    " health remaining.");
-                //player attacks monster;
-                Console.WriteLine(playerName + " attacks! The monster is defeated.");
+                    Console.WriteLine(playerName + " has " + playerHealth +
+                        " health remaining.");
+                    if (playerHealth <= 0)
+                    {
+                        Console.WriteLine("You have been defeated. \n Game Over.");
+                        Console.ReadKey();
+                        return false;
+                    }
+                    Console.ReadKey();
+
+                    //player attacks monster;
+                    Console.WriteLine(playerName + " attacks The monster.");
+
+                    monsterHealth -= playerAttack;
+                    Console.WriteLine("The monster has " + monsterHealth + " Remaining");                    
+
+                }
+
+                
             }
             else if (action == "flight" || action == "Flight")
             {
@@ -46,7 +83,8 @@ namespace Simple_RPG
                 Console.WriteLine("You flapped your arms fast enough...");
             }
 
-            Console.ReadKey();
-       }
+            return true;
+
+        }
     }
 }
